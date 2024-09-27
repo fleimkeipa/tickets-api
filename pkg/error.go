c9f2cbf63eedab07e13e6deb88956935e3dcdfc6
fleimkeipa <adem_sahin12@hotmail.com>
2024-09-27 20:29:42 +0300

    Add comments on custom error funcs

--- pkg/error.go
+++ pkg/error.go
@@ -1,11 +1,13 @@
 package pkg
 
+// Error struct defines a custom error type with an error, status code, and message.
 type Error struct {
 	err        error
 	statusCode int
 	message    string
 }
 
+// NewError creates a new instance of the Error struct.
 func NewError(err error, message string, statusCode int) *Error {
 	return &Error{
 		err:        err,
@@ -14,14 +16,17 @@ func NewError(err error, message string, statusCode int) *Error {
 	}
 }
 
+// Error implements the error interface by returning the original error message.
 func (rc *Error) Error() string {
 	return rc.err.Error()
 }
 
+// Message returns the custom error message.
 func (rc *Error) Message() string {
 	return rc.message
 }
 
+// StatusCode returns the associated HTTP status code.
 func (rc *Error) StatusCode() int {
 	return rc.statusCode
 }
